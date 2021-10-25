95d8d68c6c97c0460375c73023a9dd72f0e23ac8
vrrpizzato <vrrpizzato@gmail.com>
2021-10-25 10:06:59 -0300

    liveview setup configuration

--- mix.exs
+++ mix.exs
@@ -18,14 +18,9 @@ defmodule Curriculum.MixProject do
         "coveralls.post": :test,
         "coveralls.html": :test
       ]
-      # if you want to use espec,
-      # test_coverage: [tool: ExCoveralls, test_task: "espec"]mix credo gen.config
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Curriculum.Application, []},
@@ -33,22 +28,20 @@ defmodule Curriculum.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.3"},
+      {:phoenix, "~> 1.5.9"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_view, "~> 0.16.4"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.5.0"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
@@ -60,12 +53,6 @@ defmodule Curriculum.MixProject do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
